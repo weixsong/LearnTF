c3313279c90c6194a38cbd7289c41846f43eafa7
weixsong <watkinsong@163.com>
2017-02-17 22:57:47 +0800

    update

--- TensorFlow-Tutorials/03_net.py
+++ TensorFlow-Tutorials/03_net.py
@@ -27,15 +27,25 @@ py_x = model(X, w_h, w_o)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y)) # compute costs
 train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct an optimizer
-predict_op = tf.argmax(py_x, 1)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(py_x, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # Launch the graph in a session
 with tf.Session() as sess:
     # you need to initialize all variables
     tf.global_variables_initializer().run()
 
-    for i in range(100):
+    for i in range(20):
         for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
-        print(i, np.mean(np.argmax(teY, axis=1) ==
-                         sess.run(predict_op, feed_dict={X: teX})))
+        print("epoch %d, accuracy=%f" %
+            (i + 1, sess.run(accuracy, feed_dict={X: teX[:256], Y: teY[:256]})))
+
+    print("Optimization Finished!")
+
+    # Calculate accuracy for 256 mnist test images
+    print("Testing Accuracy: ", \
+        sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
+                                      Y: mnist.test.labels[:256],}))
