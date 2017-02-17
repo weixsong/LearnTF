69db05005927e5c916aeb12ed49d4f04b3fcdcde
weixsong <watkinsong@163.com>
2017-02-17 21:53:39 +0800

    update

--- TensorFlow-Tutorials/01_linear_regression.py
+++ TensorFlow-Tutorials/01_linear_regression.py
@@ -4,29 +4,38 @@ import tensorflow as tf
 import numpy as np
 
 trX = np.linspace(-1, 1, 101)
-trY = 2 * trX + np.random.randn(*trX.shape) * 0.33 # create a y value which is approximately linear but with some random noise
+# create a y value which is approximately linear but with some random noise
+trY = 2 * trX + np.random.randn(*trX.shape) * 0.33
 
 X = tf.placeholder("float") # create symbolic variables
 Y = tf.placeholder("float")
 
-
 def model(X, w):
-    return tf.multiply(X, w) # lr is just X*w so this model line is pretty simple
+    # lr is just X*w so this model line is pretty simple
+    return tf.multiply(X, w)
 
 
-w = tf.Variable(0.0, name="weights") # create a shared variable (like theano.shared) for the weight matrix
+# create a shared variable (like theano.shared) for the weight matrix
+w = tf.Variable(0.0, name="weights")
 y_model = model(X, w)
 
-cost = tf.square(Y - y_model) # use square error for cost function
+# use square error for cost function
+cost = tf.square(Y - y_model)
 
-train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost) # construct an optimizer to minimize cost and fit line to my data
+# construct an optimizer to minimize cost and fit line to my data
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+# Initializing the variables
+init = tf.global_variables_initializer()
 
 # Launch the graph in a session
 with tf.Session() as sess:
     # you need to initialize variables (in this case just variable W)
-    tf.global_variables_initializer().run()
+    sess.run(init)
 
     for i in range(100):
+        if i % 10 == 0:
+            print("train step %d" % i)
+
         for (x, y) in zip(trX, trY):
             sess.run(train_op, feed_dict={X: x, Y: y})
 
--- TensorFlow-Tutorials/02_logistic_regression.py
+++ TensorFlow-Tutorials/02_logistic_regression.py
@@ -10,7 +10,9 @@ def init_weights(shape):
 
 
 def model(X, w):
-    return tf.matmul(X, w) # notice we use the same model as linear regression, this is because there is a baked in cost function which performs softmax and cross entropy
+    # notice we use the same model as linear regression, 
+    # this is because there is a baked in cost function which performs softmax and cross entropy
+    return tf.matmul(X, w)
 
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -19,13 +21,18 @@ trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, 
 X = tf.placeholder("float", [None, 784]) # create symbolic variables
 Y = tf.placeholder("float", [None, 10])
 
-w = init_weights([784, 10]) # like in linear regression, we need a shared variable weight matrix for logistic regression
+# like in linear regression, we need a shared variable weight matrix for logistic regression
+w = init_weights([784, 10])
 
 py_x = model(X, w)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y)) # compute mean cross entropy (softmax is applied internally)
-train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct optimizer
-predict_op = tf.argmax(py_x, 1) # at predict time, evaluate the argmax of the logistic regression
+ # compute mean cross entropy (softmax is applied internally)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+
+# construct optimizer
+train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
+# at predict time, evaluate the argmax of the logistic regression
+predict_op = tf.argmax(py_x, 1)
 
 # Launch the graph in a session
 with tf.Session() as sess:
@@ -35,5 +42,9 @@ with tf.Session() as sess:
     for i in range(100):
         for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
+
+        if i % 10 == 0:
+            c = sess.run(cost, feed_dict={X: trX[start:end], Y: trY[start:end]})
+
         print(i, np.mean(np.argmax(teY, axis=1) ==
                          sess.run(predict_op, feed_dict={X: teX})))
