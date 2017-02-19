9e438d51b2010dfa38c03824645e367e5c127804
weixsong <watkinsong@163.com>
2017-02-19 20:20:18 +0800

    update

--- TensorFlow-Tutorials/06_autoencoder.py
+++ TensorFlow-Tutorials/06_autoencoder.py
@@ -12,14 +12,22 @@ import matplotlib.gridspec as gridspec
 def vis(images, save_name):
     dim = images.shape[0]
     n_image_rows = int(np.ceil(np.sqrt(dim)))
-    n_image_cols = int(np.ceil(dim * 1.0/n_image_rows))
-    gs = gridspec.GridSpec(n_image_rows,n_image_cols,top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
-    for g,count in zip(gs,range(int(dim))):
+    n_image_cols = int(np.ceil(dim * 1.0 / n_image_rows))
+    gs = gridspec.GridSpec(n_image_rows,
+                           n_image_cols,
+                           top=1.,
+                           bottom=0.,
+                           right=1.,
+                           left=0.,
+                           hspace=0.,
+                           wspace=0.)
+    for g, count in zip(gs, range(int(dim))):
         ax = plt.subplot(g)
         ax.imshow(images[count,:].reshape((28,28)))
         ax.set_xticks([])
         ax.set_yticks([])
     plt.savefig(save_name + '_vis.png')
+
 
 mnist_width = 28
 n_visible = mnist_width * mnist_width
@@ -59,6 +67,7 @@ Z = model(X, mask, W, b, W_prime, b_prime)
 cost = tf.reduce_sum(tf.pow(X - Z, 2))  # minimize squared error
 train_op = tf.train.GradientDescentOptimizer(0.02).minimize(cost)  # construct an optimizer
 predict_op = Z
+
 # load MNIST data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
@@ -68,14 +77,15 @@ with tf.Session() as sess:
     # you need to initialize all variables
     tf.global_variables_initializer().run()
 
-    for i in range(100):
-        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
+    for i in range(10):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX) + 1, 128)):
             input_ = trX[start:end]
             mask_np = np.random.binomial(1, 1 - corruption_level, input_.shape)
             sess.run(train_op, feed_dict={X: input_, mask: mask_np})
 
         mask_np = np.random.binomial(1, 1 - corruption_level, teX.shape)
         print(i, sess.run(cost, feed_dict={X: teX, mask: mask_np}))
+
     # save the predictions for 100 images
     mask_np = np.random.binomial(1, 1 - corruption_level, teX[:100].shape)
     predicted_imgs = sess.run(predict_op, feed_dict={X: teX[:100], mask: mask_np})
