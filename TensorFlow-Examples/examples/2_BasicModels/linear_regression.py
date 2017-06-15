df897da74b9345028fd0374fa13fcf45149164f9
weixsong <watkinsong@163.com>
2017-06-15 15:06:51 +0800

    update

--- TensorFlow-Examples/examples/2_BasicModels/linear_regression.py
+++ TensorFlow-Examples/examples/2_BasicModels/linear_regression.py
@@ -18,10 +18,10 @@ training_epochs = 1000
 display_step = 50
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+train_X = numpy.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                         7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+train_Y = numpy.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                         2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
 n_samples = train_X.shape[0]
 
 # tf Graph Input
@@ -33,7 +33,7 @@ W = tf.Variable(rng.randn(), name="weight")
 b = tf.Variable(rng.randn(), name="bias")
 
 # Construct a linear model
-pred = tf.add(tf.mul(X, W), b)
+pred = tf.add(tf.multiply(X, W), b)
 
 # Mean squared error
 cost = tf.reduce_mean(tf.pow(pred - Y, 2) / 2)
@@ -54,18 +54,21 @@ with tf.Session() as sess:
             sess.run(optimizer, feed_dict={X: x, Y: y})
 
         # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-                "W=", sess.run(W), "b=", sess.run(b))
+        if (epoch + 1) % display_step == 0:
+            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c),
+                  "W=", sess.run(W), "b=", sess.run(b))
 
     print("Optimization Finished!")
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    print("Training cost=", training_cost, "W=",
+          sess.run(W), "b=", sess.run(b), '\n')
 
     # Graphic display
     plt.plot(train_X, train_Y, 'ro', label='Original data')
     plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.ylabel('Y')
+    plt.xlabel('X')
     plt.legend()
     plt.show()
 
@@ -83,5 +86,7 @@ with tf.Session() as sess:
 
     plt.plot(test_X, test_Y, 'bo', label='Testing data')
     plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.ylabel('Y')
+    plt.xlabel('X')
     plt.legend()
     plt.show()
