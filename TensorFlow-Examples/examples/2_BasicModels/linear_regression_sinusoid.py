5f16c43b27b272d8616efc62a73dc973209bd6da
weixsong <watkinsong@163.com>
2017-06-15 17:16:12 +0800

    update

--- TensorFlow-Examples/examples/2_BasicModels/linear_regression_sinusoid.py
+++ TensorFlow-Examples/examples/2_BasicModels/linear_regression_sinusoid.py
@@ -14,12 +14,12 @@ import matplotlib.pyplot as plt
 rng = numpy.random
 
 # Parameters
-learning_rate = 0.001
+learning_rate = 0.0001
 training_steps = 10000
 display_step = 50
 sample_number = 1000
-linspace_start = 0
-linespace_end = 4 * np.pi
+linspace_start = 2
+linespace_end = 4 * np.pi + linspace_start
 batch_size = 20
 
 # training data
@@ -41,9 +41,21 @@ n_samples = sample_number
 # the feature number
 # here we constuct a few other feature by train_x
 origin_x = train_x
-for i in range(2):
-    temp = np.power(origin_x, i + 2)
-    train_x = np.column_stack((train_x, temp))
+x_power = np.power(origin_x, 2)
+train_x = np.column_stack((train_x, x_power))
+
+x_log = np.log(origin_x)
+train_x = np.column_stack((train_x, x_log))
+
+x_exp = np.exp(origin_x)
+train_x = np.column_stack((train_x, x_exp))
+
+x_xlogx = np.multiply(origin_x, x_log)
+train_x = np.column_stack((train_x, x_xlogx))
+
+x_sqrt = np.sqrt(origin_x)
+train_x = np.column_stack((train_x, x_sqrt))
+
 
 # normalize data
 train_x = (train_x - train_x.mean(0)) / train_x.std(0)
@@ -52,11 +64,12 @@ print("training data shape")
 print(train_x.shape)
 
 # tf Graph Input
-X = tf.placeholder("float", [None, 3])
+X = tf.placeholder("float", [None, 6])
 Y = tf.placeholder("float", [None, 1])
 
 # Set model weights
-W = tf.Variable(tf.constant([(10.0,), (20.0,), (2.0,)]), name="weight")
+W = tf.Variable(tf.constant(
+    [(50.0,), (40.0,), (30.0,), (20.0,), (10.0,), (5.0,)]), name="weight")
 b = tf.Variable(rng.randn(), name="bias")
 
 # Construct a linear model
@@ -95,7 +108,7 @@ with tf.Session() as sess:
     plt.plot(origin_x, train_y, 'r', label='Original data')
     prediction_y = sess.run(pred, feed_dict={X: train_x, Y: train_y})
     print(prediction_y.shape)
-    plt.plot(train_x, prediction_y, 'b', label='Fitted line')
+    plt.plot(origin_x, prediction_y, 'b', label='Fitted line')
     plt.ylabel('Y')
     plt.xlabel('X')
     plt.legend()
