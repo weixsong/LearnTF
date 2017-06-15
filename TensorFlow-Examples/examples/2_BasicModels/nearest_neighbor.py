359a0ed49c6f00d8e5450e77e413647dae16bdd0
weixsong <watkinsong@163.com>
2017-06-15 17:36:16 +0800

    update

--- TensorFlow-Examples/examples/2_BasicModels/nearest_neighbor.py
+++ TensorFlow-Examples/examples/2_BasicModels/nearest_neighbor.py
@@ -17,8 +17,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 # In this example, we limit mnist data
-Xtr, Ytr = mnist.train.next_batch(5000) #5000 for training (nn candidates)
-Xte, Yte = mnist.test.next_batch(200) #200 for testing
+Xtr, Ytr = mnist.train.next_batch(5000)  # 5000 for training (nn candidates)
+Xte, Yte = mnist.test.next_batch(200)  # 200 for testing
 
 # tf Graph Input
 xtr = tf.placeholder("float", [None, 784])
@@ -26,7 +26,8 @@ xte = tf.placeholder("float", [784])
 
 # Nearest Neighbor calculation using L1 Distance
 # Calculate L1 Distance
-distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+distance = tf.reduce_sum(
+    tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
 # Prediction: Get min distance index (Nearest neighbor)
 pred = tf.arg_min(distance, 0)
 
@@ -44,10 +45,10 @@ with tf.Session() as sess:
         # Get nearest neighbor
         nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i, :]})
         # Get nearest neighbor class label and compare it to its true label
-        print("Test", i, "Prediction:", np.argmax(Ytr[nn_index]), \
-            "True Class:", np.argmax(Yte[i]))
+        print("Test", i, "Prediction:", np.argmax(Ytr[nn_index]),
+              "True Class:", np.argmax(Yte[i]))
         # Calculate accuracy
         if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
-            accuracy += 1./len(Xte)
+            accuracy += 1. / len(Xte)
     print("Done!")
     print("Accuracy:", accuracy)
